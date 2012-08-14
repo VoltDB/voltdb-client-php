97aa40c6c08134620c1628c64d6e8fd94f30c3d4
Ning Shi <nshi@voltdb.com>
2012-08-14 15:24:49 -0400

    Remove old client code and reorganize the directory structure.

--- src/scripts/servercheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ `pgrep -f org.voltdb.VoltDB | wc -l` -ne 0 ]; then
-    echo "There is a VoltDB server running already that you should stop before continuing."
-    echo "You can kill it using \"ant serverkill\" (Warning: kills ALL local VoltDB servers)."
-    exit 1
-fi
