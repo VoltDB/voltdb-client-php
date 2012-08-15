a449d29e484033fd9423374df3472f7647c6fca2
Ning Shi <nshi@voltdb.com>
2012-08-15 12:58:35 -0400

    Update the voter_apache.php script to use the new client code.

--- examples/voter_apache.php
+++ examples/voter_apache.php
@@ -1,26 +1,9 @@
 <?php
 
-require('voltdb.php');
-
 // EDIT 'localhost' below to specify one or two
 // hostnames running the VoltDB voter sample application.
 $host1 = 'localhost';
 $host2 = 'localhost';
-
-class AsyncCallback extends \volt\ProcedureCallback {
-    public static $goodVotes = 0;
-    public static $badVotes = 0;
-    function callback($response) {
-        if ($response->failure()) {
-            AsyncCallback::$badVotes++;
-        } else {
-            AsyncCallback::$goodVotes++;
-        }
-    }
-}
-
-// php not-reentrant. callback is reused.
-$callback = new AsyncCallback();
 
 // some voter configuration.
 $maxVotesPerPhoneNumber = 5;
@@ -49,31 +32,32 @@ if ($client < 50) {
 
 // Use a client that has a cached connection from the pool.
 try {
-    $voltClient = \volt\Client::createFromPool($hostname, '', '', null, 21212);
+    $voltClient = new VoltClient();
+    $voltClient->connect($hostname, '', '');
 } catch (Exception $e) {
     print($e->getMessage() . "\n");
     exit(1);
 }
 
-// declare the vote procedure
-$parameters = new \volt\Parameters();
-$parameters->push(new \volt\Parameter(\volt\voltdb::WIRE_TYPE_BIGINT));
-$parameters->push(new \volt\Parameter(\volt\voltdb::WIRE_TYPE_TINYINT));
-$parameters->push(new \volt\Parameter(\volt\voltdb::WIRE_TYPE_BIGINT));
-$procedure = new \volt\Procedure('Vote', $parameters);
+$goodVotes = 0;
+$badVotes = 0;
 
 // vote 5 times
 for ($i = 0; $i < 5; $i++) {
     $phoneNumber = mt_rand(0, 9999999999);
     $contestantNumber = mt_rand(0, $maxContestants) * mt_rand(0, $maxContestants) % $maxContestants + 1;
-    $procedure->params()->addInt64($phoneNumber)->addInt8($contestantNumber)->addInt64($maxVotesPerPhoneNumber);
-    $voltClient->invoke($procedure, $callback);
+    try {
+        $resp = $voltClient->invoke("Vote", array(strval($phoneNumber), strval($contestantNumber), strval($maxVotesPerPhoneNumber)));
+        if ($resp->statusCode() == VoltInvocationResponse::SUCCESS) {
+            $goodVotes++;
+        }
+    } catch (Exception $e) {
+        $badVotes++;
+        echo 'Exception '. $e->getCode() . '\n';
+        continue;
+    }
 }
 
-// collect responses
-while (!$voltClient->drain()) {
-}
-
-printf("<html><body>Accepted votes:" . AsyncCallback::$goodVotes .
-       " Failed votes:" . AsyncCallback::$badVotes . "</body></html>");
+printf("<html><body>Accepted votes:" . $goodVotes .
+       " Failed votes:" . $badVotes . "</body></html>\n");
 
