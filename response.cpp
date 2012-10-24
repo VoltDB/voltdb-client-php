c334dee36cd97dcece42365e0f017149b40c7d51
Bas van Beek <bas@xacte.com>
2012-10-24 12:03:44 -0400

    Fix compilation issues on PHP 5.4.
    Signed-off-by: Ning Shi <nshi@voltdb.com>

--- response.cpp
+++ response.cpp
@@ -74,8 +74,12 @@ zend_object_value voltresponse_create_handler(zend_class_entry *type TSRMLS_DC)
 
     ALLOC_HASHTABLE(obj->std.properties);
     zend_hash_init(obj->std.properties, 0, NULL, ZVAL_PTR_DTOR, 0);
+#if PHP_VERSION_ID < 50399
     zend_hash_copy(obj->std.properties, &type->default_properties,
                    (copy_ctor_func_t)zval_add_ref, (void *)&tmp, sizeof(zval *));
+#else
+    object_properties_init(&(obj->std), type);
+#endif
 
     retval.handle = zend_objects_store_put(obj, NULL,
                                            voltresponse_free, NULL TSRMLS_CC);
